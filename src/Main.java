56f0bf4e5aaad9e1cf88b6437e4f6b22242eed0b
yagur <yagur@DESKTOP-HJ1O55H>
2026-02-04 22:57:01 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 void main() {
     Path path = Paths.get("simple.txt");
     String str = "DDLWA: __@#1";
@@ -7,5 +9,18 @@ void main() {
         System.out.println("file create");
     }catch (IOException e){
         System.out.println("error ");
+    }
+    Path filePath2 = Paths.get("log.txt");
+    String newEntry = "Новая запись в логе: " + java.time.LocalDateTime.now();
+
+    try {
+        Files.write(
+                filePath2,
+                List.of(newEntry),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND
+        );
+    } catch (IOException e) {
+        System.out.println("Ошибка при добавлении записи: " + e.getMessage());
     }
 }
